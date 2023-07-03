cb4dda60aff174d5f4483aa1cb35a54409939638
Simone-Damico <simodamoco1992@gmail.it>
2023-07-03 17:51:02 +0200

    Add embeddings model

--- TextSampler.py
+++ TextSampler.py
@@ -2,6 +2,8 @@ from clustering.algorithms import perform_cluster
 from clustering.evaluation import evaluate_clusters
 from clustering.utils import plot_clusters, pca
 
+from Embedding import perform_embedding
+
 from typing import List, Dict
 import pandas as pd
 import numpy as np
@@ -42,38 +44,45 @@ class SampleView:
         self.clu_parameters = None
 
     def sample(self, frac: float, sampling_var: List[str], text_var: str = None,
-               use_pca: bool = True, n_clusters: int = None,
-               emb_algo: str = 'Doc2Vec', vector_size: int = 100, window: int = 5, min_count: int = 1, workers: int = 1,
-               emb_epochs: int = 10,
-               cluster_algo: str = 'kmeans', seed: int = 42):
+               use_pca: bool = True, cluster_algo: str = 'kmeans', n_clusters: int = None,
+               emb_algo: str = 'Doc2Vec', vector_size: int = 100, window: int = 5,
+               min_count: int = 1, workers: int = 3, emb_epochs: int = 10,
+               seed: int = 42, **kwargs):
 
         self.seed = seed
         self.sampling_var = set(sampling_var)
         self.text_var = text_var
 
         if text_var:
-            self.sampling_var.add(text_var)
-            X, _ = make_blobs(n_samples=10000, n_features=4, random_state=1)
-            self.vec = pd.DataFrame(X)
-            # Embedding.perform_embedding(corpus, emb_algo, vector_size, window, min_count, workers, emb_epochs)
+            #X, _ = make_blobs(n_samples=10000, n_features=4, random_state=1)
+            self.vec, self.emb_model_params = perform_embedding(self.data[text_var], emb_algo=emb_algo, vector_size=vector_size, window=window,
+                                         min_count=min_count, workers=workers, emb_epochs=emb_epochs, **kwargs)
 
             if use_pca:
                 self.variables = pca(self.vec, self.seed)
+
             else:
                 self.variables = self.vec
 
             self.data['cluster_labels'], self.clu_parameters = perform_cluster(data=self.variables,
                                                                                cluster_algo=cluster_algo,
+                                                                               n_clusters=n_clusters,
                                                                                seed=seed)
-
+            self.sampling_var.add('cluster_labels')
+            self.sampling_var = list(self.sampling_var)
+        self.sample = self.data.groupby(self.sampling_var).apply(lambda x: x.sample(int(round(x.shape[0]*0.8, 0))))
         # self.sample, _ = train_test_split(self.data, train_size=frac, stratify=self.data[sampling_var],
         # random_state=seed)
+        return self.sample
 
     def cluster_evaluation(self, metric: str = 'silhouette_score') -> float:
         return evaluate_clusters(self.vec.values, self.data['cluster_labels'], metric)
 
     def plot_clusters(self):
         plot_clusters(self.variables, self.data['cluster_labels'])
+
+    def get_embeddings_params(self) -> Dict:
+        return self.emb_model_params
 
     def get_embeddings_vec(self) -> np.ndarray:
         return self.vec.values
