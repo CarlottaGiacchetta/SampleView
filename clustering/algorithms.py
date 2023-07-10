57dd99c363c0e20819433a30f3cf0e7b35e00f9a
Simone-Damico <simodamoco1992@gmail.it>
2023-07-10 16:00:47 +0200

    Added plot for SOM clustering

--- clustering/algorithms.py
+++ clustering/algorithms.py
@@ -7,7 +7,6 @@ from clustering.utils import get_best_n_clusters
 
 
 def _kmeans_clustering(data: pd.DataFrame, n_clusters: int = None, seed: int = None) -> Tuple[Any, dict[str, int]]:
-
     if not n_clusters:
         n_clusters = get_best_n_clusters(data, seed)
 
@@ -19,6 +18,8 @@ def _kmeans_clustering(data: pd.DataFrame, n_clusters: int = None, seed: int = N
 
 def _som_clustering(data: pd.DataFrame, map_size: Tuple[int, int] = (5, 5),
                     n_epochs: int = 100, seed: int = None) -> Tuple[list[Any], dict[str, int | tuple[int, int]]]:
+    all_cells = [(x, y) for x in range(0, map_size[0]) for y in range(0, map_size[1])]
+
     # Dimensioni della mappa SOM -> ne abbiamo scelta una di 25 celle in modo tale da avere 25 potenziali clusters.
     data = data.values
     # Si dice la dimensione della som, la grandezza dei vettori della som, la forma delle celle
@@ -30,22 +31,15 @@ def _som_clustering(data: pd.DataFrame, map_size: Tuple[int, int] = (5, 5),
         winner = som.winner(input_vector)
         winners[ind] = winner
 
-    # Ottieni i valori unici dal dizionario
-    value_enum = {value: enum for enum, value in enumerate(set(winners.values()))}
+    labels = {k: all_cells.index(winners[k]) for k in winners}
 
-    # Mappa i valori del dizionario originale all'enumerazione dei valori unici
-    mapped_dict = {key: value_enum[value] for key, value in winners.items()}
-
-    labels = [value for key, value in sorted(mapped_dict.items())]
-
-    return labels, {'algo': 'SOM', 'n_clusters': len(set(labels)),
-                    'map_size': map_size, 'n_epochs': n_epochs, 'seed': seed}
+    return list(labels.values()), {'algo': 'SOM', 'n_clusters': len(set(labels)),
+                                   'map_size': map_size, 'n_epochs': n_epochs, 'seed': seed}
 
 
 def perform_cluster(data: pd.DataFrame,
                     cluster_algo: str,
                     **kwargs) -> Tuple[list[int], Dict[str, int | tuple[int, int]]]:
-
     if cluster_algo == 'kmeans':
         clu_parameters = _kmeans_clustering.__code__.co_varnames[:_kmeans_clustering.__code__.co_argcount]
         kwargs = {key: value for key, value in kwargs.items() if key in clu_parameters}
--- clustering/utils.py
+++ clustering/utils.py
@@ -4,7 +4,7 @@ from numpy import cumsum, argmax
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import silhouette_score
-
+from collections import Counter
 import matplotlib.pyplot as plt
 
 
@@ -20,29 +20,6 @@ def pca(df: pd.DataFrame, seed, var_threshold: float = 0.95) -> pd.DataFrame:
                           columns=[f'PC_{i}' for i in range(1, num_components + 1)],
                           index=df.index)
     return df_pca
-
-
-def plot_clusters(df: pd.DataFrame, labels: list) -> None:
-    X = df.values
-
-    # Genera un insieme di colori unici per i cluster
-    unique_labels = np.unique(labels)
-    n_clusters = len(unique_labels)
-    colors = plt.cm.get_cmap('tab10', n_clusters)
-
-    # Crea un grafico per ogni cluster
-    for i, label in enumerate(unique_labels):
-        cluster_points = X[labels == label]
-        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color=colors(i), label=f'Cluster {label}')
-
-    # Personalizza il grafico
-    plt.xlabel('Feature 1')
-    plt.ylabel('Feature 2')
-    plt.title('Clustering Plot')
-    plt.legend()
-
-    # Mostra il grafico
-    plt.show()
 
 
 def get_best_n_clusters(data: pd.DataFrame, seed: int = 42) -> int:
@@ -63,3 +40,54 @@ def get_best_n_clusters(data: pd.DataFrame, seed: int = 42) -> int:
             best_n_clusters = n_clusters
 
     return best_n_clusters
+
+
+def plot_som_clusters(labels, map_size):
+    all_cells = [(x, y) for x in range(0, map_size[0]) for y in range(0, map_size[1])]
+    stats_map = [x for i, x in enumerate(all_cells) if i in labels]
+    occurrences = Counter(labels)
+    stats_map = np.zeros(map_size)
+    for i, c in enumerate(all_cells):
+        stats_map[c[0], c[1]] = occurrences[i]
+
+    stats_map_normalized = stats_map / np.sum(stats_map)
+    # (stats_map - np.min(stats_map)) / (np.max(stats_map) - np.min(stats_map))
+    print(stats_map_normalized.T)
+    plt.figure(figsize=(map_size[0], map_size[1]))
+    plt.pcolormesh(stats_map_normalized.T)
+    plt.colorbar(label='% Statistical Units')
+    plt.xlabel('Dimension 1')
+    plt.ylabel('Dimension 2')
+    plt.title('SOM Map')
+
+    # se non vuoi che si veda il numero di unità statstiche associate elimina
+    for i in range(map_size[0]):
+        for j in range(map_size[1]):
+            if stats_map[i, j] > 0:
+                plt.text(i + 0.5, j + 0.5, f'{int(stats_map[i, j])} ', ha='center', va='center', color='white',
+                         fontsize=8)
+
+    plt.show()
+
+
+def plot_k_means_clusters(df: pd.DataFrame, labels: list) -> None:
+    X = df.values
+
+    # Genera un insieme di colori unici per i cluster
+    unique_labels = np.unique(labels)
+    n_clusters = len(unique_labels)
+    colors = plt.cm.get_cmap('tab10', n_clusters)
+
+    # Crea un grafico per ogni cluster
+    for i, label in enumerate(unique_labels):
+        cluster_points = X[labels == label]
+        plt.scatter(cluster_points[:, 0], cluster_points[:, 1], color=colors(i), label=f'Cluster {label}')
+
+    # Personalizza il grafico
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('Clustering Plot')
+    plt.legend()
+
+    # Mostra il grafico
+    plt.show()
